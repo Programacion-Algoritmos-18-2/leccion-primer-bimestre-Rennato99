821f0a45f4cab599878fca1e871030b6720c37d2
Renato Balcazar <renatojbl99@gmail.com>
2018-11-09 11:44:15 -0500

    Subiendo el ejercicio completo en python y comentado

--- ejercicio1/mipaquete/modelado.py
+++ ejercicio1/mipaquete/modelado.py
@@ -1,14 +1,18 @@
 #clases.py
 
-class Empleado():
+"""
+Creamos la clase padre que se llama Empleado
+"""
+class Empleado(object): 
 	
+	# Constructor
 	def __init__(self):
-
 		self.nombre = ""
 		self.apellido = ""
 		self.cedula = ""
 		self.comision_fija = 0
 
+	# Métodos set
 	def agregar_nombre(self, nombre):
 		self.nombre = nombre
 
@@ -21,6 +25,7 @@ class Empleado():
 	def agregar_comision_fija(self, comision_fija):
 		self.comision_fija = comision_fija
 
+	# Métodos get
 	def obtener_nombre(self):
 		return self.nombre
 
@@ -30,31 +35,127 @@ class Empleado():
 	def obtener_cedula(self):
 		return self.cedula
 
-	def obtener_comision_sfija(self):
+	def obtener_comision_fija(self):
 		return self.comision_fija
 
+	# Método para presentar los datos
 	def presentar_datos(self):
-		cadena = ("Informacion de %s %s\n\tCEDULA: %s" % (self.obtener_nombre(), self.apellido, self.obtener_cedula()))
+		cadena = ("\n----------\nInformacion de %s %s\n\tCEDULA: %s" % (self.obtener_nombre(), self.apellido, self.obtener_cedula()))
 		return cadena
 
+
+"""
+Creamos la clase EmpleadoFijo que hereda de Empleado
 """
 class EmpleadoFijo(Empleado):
 
-	def __init__(self, arg):
+	# Constructor que agrega los atributos de la clase padre y aparte dos más
+	def __init__(self):
 		super(EmpleadoFijo, self).__init__()
 		self.sueldo_fijo = 0
 		self.descuento_seguro = 0
 
-	def setSueldoFijo(self, sueldo_fijo):
+	# Métodos set
+	def agregar_sueldo_fijo(self, sueldo_fijo):
 		self.sueldo_fijo = sueldo_fijo
 
-	def setDescuentoSeguro(self, descuento_seguro):
+	def agregar_descuento_seguro(self, descuento_seguro):
 		self.descuento_seguro = descuento_seguro
 
-	def getSueldoFijo(self):
+	# Métodos get
+	def obtener_sueldo_fijo(self):
 		return self.sueldo_fijo
 
-	def getDescuentoSeguro(self):
+	def obtener_descuento_seguro(self):
 		return self.descuento_seguro		
 
+	# Método para calcular el sueldo final
+	def calcular_sueldo_final(self):
+		valor = (self.obtener_sueldo_fijo()*(100-self.obtener_descuento_seguro())) + self.obtener_comision_fija()
+		return valor
+
+	# Método para presentar los datos
+	def presentar_datos(self):
+		"""
+		La siguiente cadena llama une la cadena del método de la clase padre "presentar_datos" con el sueldo_fijo, descuento_seguro y el sueldo_final
+		"""
+		cadena = ("%s\nEmpleado Fijo\nSueldo fijo: %.2f\nDescuento seguro: %.2f\nSueldo final: %.2f\n----------\n"  % (super(EmpleadoFijo, self).presentar_datos(), self.obtener_sueldo_fijo(), self.obtener_descuento_seguro(), self.calcular_sueldo_final()))
+		return cadena
+
+
 """
+Creamos la clase EmpleadoPorHoras que hereda de Empleado
+"""
+class EmpleadoPorHoras(Empleado):
+
+	# Constructor que agrega los atributos de la clase padre y aparte dos más
+	def __init__(self):
+		super(EmpleadoPorHoras, self).__init__()
+		self.numero_horas = 0
+		self.valor_horas = 0
+
+	# Métodos set
+	def agregar_numero_horas(self, numero_horas):
+		self.numero_horas = numero_horas
+
+	def agregar_valor_horas(self, valor_horas):
+		self.valor_horas = valor_horas
+
+	# Metodos get
+	def obtener_numero_horas(self):
+		return self.numero_horas
+
+	def obtener_valor_horas(self):
+		return self.valor_horas
+
+	# Método para calcular el sueldo final
+	def calcular_sueldo_final(self):
+		valor = self.obtener_numero_horas() * self.obtener_valor_horas() + self.obtener_comision_fija()
+		return valor
+
+	# Método para presentar los datos
+	def presentar_datos(self):
+		"""
+		La siguiente cadena llama une la cadena del método de la clase padre "presentar_datos" con el numero_horas, valor_horas y el sueldo_final
+		"""
+		cadena = ("%s\nEmpleado Por Horas\nNumero horas: %.2f\nValor hora: %.2f\nSueldo final: %.2f\n----------\n"  % (super(EmpleadoPorHoras, self).presentar_datos(), self.obtener_numero_horas(), self.obtener_valor_horas(), self.calcular_sueldo_final()))
+		return cadena
+
+
+"""
+Creamos la clase EmpleadoPorSemana que hereda de Empleado
+"""
+class EmpleadoPorSemana(Empleado):
+	
+	# Constructor que agrega los atributos de la clase padre y aparte dos más
+	def __init__(self):
+		super(EmpleadoPorSemana, self).__init__()
+		self.numero_semanas = 0
+		self.valor_semanal = 0
+
+	# Métodos sete
+	def agregar_numero_semanas(self, numero_semanas):
+		self.numero_semanas = numero_semanas
+
+	def agregar_valor_semanal(self, valor_semanal):
+		self.valor_semanal = valor_semanal
+
+	# Métodos get
+	def obtener_numero_semanas(self):
+		return numero_semanas
+
+	def obtener_valor_semanal(self):
+		return valor_semanal
+
+	# Método para calcular el sueldo final
+	def calcular_sueldo_final(self):
+		valor = self.obtener_numero_semanas() * self.obtener_valor_semanal() + self.obtener_comision_fija()
+		return valor
+
+	# Método para presentar los datos
+	def presentar_datos(self):
+		"""
+		La siguiente cadena llama une la cadena del método de la clase padre "presentar_datos" con el numero_semanas, valor_semanal y el sueldo_final
+		"""
+		cadena = ("%s\nEmpleado Por Semana\nNumero semanas: %.2f\nValor semanal: %.2f\nSueldo final: %.2f\n----------\n"  % (super(EmpleadoPorSemana, self).presentar_datos(), self.obtener_numero_semanas(), self.obtener_valor_semanal(), self.calcular_sueldo_final()))
+		return cadena
--- ejercicio1/run.py
+++ ejercicio1/run.py
@@ -2,9 +2,41 @@
 from mipaquete.modelado import *
 
 
-# Cread Empleado
-e = Empleado()					# Creamos un objeto empleado
-e.agregar_nombre("Luis")		# Agregamos valor al atributo nombre
-e.agregar_apellido("Benitez")	# Agregamos valor al atributo apellido
-e.agregar_cedula("1110001")		# Agregamos valor al atributo cedula
-print(e.presentar_datos())		# Presentamos los datos
+# Creamos Empleado
+empleado = Empleado()					# Creamos un objeto empleado
+empleado.agregar_nombre("Luis")			# Agregamos valor al atributo nombre
+empleado.agregar_apellido("Benitez")	# Agregamos valor al atributo apellido
+empleado.agregar_cedula("1110001")		# Agregamos valor al atributo cedula
+print(empleado.presentar_datos())		# Presentamos los datos
+
+# Creamos un empleado fijo
+empleado_fijo = EmpleadoFijo()  					# Creamos un objeto empleadoFijo
+empleado_fijo.agregar_nombre("Ana")					# Agregamos el nombre Ana
+empleado_fijo.agregar_apellido("Diaz")				# Agregamos el apellido Diaz
+comision = float(input("Ingrese comision: "))		
+empleado_fijo.agregar_comision_fija(comision)		# Agregamos comision
+empleado_fijo.agregar_sueldo_fijo(300.2)			# Agregamos un sueldo fijo
+empleado_fijo.agregar_descuento_seguro(10)			# Agregamos valor al seguro
+print(empleado_fijo.presentar_datos())				# Presentamos los datos
+
+#Creamos un empleado por horas
+empleado_por_horas = EmpleadoPorHoras()				# Creamos un objeto empleado por Horas
+nombre = input("Ingrese su nombre: ")
+empleado_por_horas.agregar_nombre(nombre)			# Agregamos el nombre 
+empleado_por_horas.agregar_apellido("Andrade")		# Agregamos el apellido Andrade
+empleado_por_horas.agregar_cedula("112233")			# Agregamos valor al atributo cedula
+empleado_por_horas.agregar_numero_horas(15)			# Agregamos alor al atributo numero_horas
+empleado_por_horas.agregar_valor_horas(20.2)		# Agregamos alor al atributo valor_horass
+print(empleado_por_horas.presentar_datos())			# Presentamos los datos
+
+"""
+#Creamos un empleado por semana
+empleado_por_semana = EmpleadoPorSemana()
+empleado_por_semana.agregar_nombre("Gerson")
+empleado_por_semana.agregar_apellido("Santos")
+empleado_por_semana.agregar_cedula("1105137234")
+empleado_por_semana.agregar_comision_fija(1)
+empleado_por_semana.agregar_numero_semanas(1)
+empleado_por_semana,agregar_valor_semanal(1)
+print(empleado_por_semana.presentar_datos())
+"""
